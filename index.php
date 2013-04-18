fb39cc56399c45a99e36aa55cadd4f7b6de88a9b
David Swanson <swanson.david@gmail.com>
2013-04-17 18:12:50 -0700

    made the box wider

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <p><a href="test.php">try a server request</a></p>
 <form action="test.php" method="post">
   <div>
-    Query: <input type="text" name="query" /> <br />
+    Query: <input type="text" name="query" width="500" /> <br />
     <input type="submit" />
   <div>
 </form>
