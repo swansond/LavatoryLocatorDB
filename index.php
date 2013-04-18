b38d62be8a2f04b92952ef5912106532e7715644
David Swanson <swanson.david@gmail.com>
2013-04-17 17:54:18 -0700

    added php to write queries

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 <body>
 <h1>Test page</h1>
 <p><a href="test.php">try a server request</a></p>
+<form action="test.php" method="post">
+  <div>
+    Query: <input type="text" name="query" /> <br />
+    <input type="submit" />
+  <div>
+</form>
 </body>
 </html>
 
--- test.php
+++ test.php
@@ -4,14 +4,18 @@
 function pg_connection_string() {
 	return "dbname=dc9160dninujhs host=ec2-23-21-85-233.compute-1.amazonaws.com port=5432 user=todxuvhszxmpnh password=fKeepFUZjdUgrXhifoZsTDsyFG sslmode=require";
 }
+$query = "select * from team;";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$query = $_POST["query"];
+}
 # Establish db connection
 $db = pg_connect(pg_connection_string());
 if (!$db) {
    echo "Database connection error.";
    exit;
 }
-$result = pg_query($db, "SELECT * from team;");
+$result = pg_query($db, $query);
 
 $myarray = array();
 while ($row = pg_fetch_row($result)) {
