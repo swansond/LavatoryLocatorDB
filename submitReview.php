529a315eaa7823d0bff4bae52417e8bd1e11b7f2
aasav <aasav@cs.uw.edu>
2013-05-05 11:19:19 -0700

    Fetching and submitting user reviews

--- submitReview.php
+++ submitReview.php
@@ -1,28 +1,61 @@
 <?php
-# Page accepts POST parameters to add a review to the database
+/**
+*  Page accepts POST parameters to update a user's review, 
+*  or add a review if one does not exist.
+*  @author Aasav Prakash
+*/
 
-$toiletID = $_POST['toilet'];
+$lID = $_POST['lid'];
 $rating = $_POST['rating'];
 $review = pg_escape_string($_POST['review']);
-$userID = $_POST['userID'];
+$userID = $_POST['uid'];
+$now = new DateTime().getTimestamp();
 
-if(!$toiletID || !$rating || !$review) {
-	die('Invalid parameters');
+if (!$lID || !$rating || !$review || !$userID) {
+    header('HTTP/1.1 400 Invalid Request');
+	die('HTTP/1.1 400 Invalid Request: Missing required parameters');
 }
 
+/**
+ * @return the string used to connect to the postgres server
+ */
 function pg_connection_string() {
-	return "dbname=dc9160dninujhs host=ec2-23-21-85-233.compute-1.amazonaws.com port=5432 user=todxuvhszxmpnh password=fKeepFUZjdUgrXhifoZsTDsyFG sslmode=require";
+	return "dbname=dc9160dninujhs host=ec2-23-21-85-233.compute-1.amazonaws.com
+    port=5432 user=todxuvhszxmpnh
+    password=fKeepFUZjdUgrXhifoZsTDsyFG sslmode=require";
 }
 
 $db = pg_connect(pg_connection_string());
 if (!$db) {
-   die('Database connection error');
+   header('HTTP/1.1 500 Server Error');
+   die('HTTP/1.1 500 Server Error: unable to connect to the server');
 }
 
-$now = new DateTime().getTimestamp();
-$query = "INSERT INTO review VALUES(1, $toiletID, $userID, $now, $review, $rating, 0)";
-$result = pg_query($db, $query);
-if(!$result) {
-	die('SQL Query error');
+// Check if the user already has a review
+$checkQuery = "SELECT * FROM Review WHERE lavatory_id='$lID' AND user_id='$userID'";
+$checkResult = pg_query($db, $checkQuery);
+if (!$checkResult) {
+    header('HTTP/1.1 500 Server Error');
+    die('HTTP/1.1 500 Server Error: unable to query the server');
 }
-?>
+
+if (pg_num_rows($checkResult) == 0) {
+    // User does not have review; add a new one
+    $query = "INSERT INTO Review (lavatory_id, user_id, datetime, review, 
+                                  rating, helpfulness)
+              VALUES($lID, $userID, $now, $review, $rating, 0)";
+    $result = pg_query($db, $query);
+    if (!$result) {
+        header('HTTP/1.1 500 Server Error');
+        die('HTTP/1.1 500 Server Error: unable to query the server');
+    }    
+} else {
+    // User has an existing review; update it
+    $query = "UPDATE Review SET datetime='$now', review='$review', rating='$rating'
+              WHERE lavatory_id='$lID' AND user_id='$userID'";
+    $result = pg_query($db, $query);
+    if (!$result) {
+        header('HTTP/1.1 500 Server Error');
+        die('HTTP/1.1 500 Server Error: unable to query the server');
+    }
+}
