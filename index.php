87cf53f70d7708c20dad283f8d0baa3de9551b19
David Jung <xdavidjung@gmail.com>
2013-05-06 18:57:09 -0700

    added link to alpha release

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 </head>
 <body>
 <h1>Lavatory Locator</h1>
-<p><a href="LavatoryLocator.apk"</a>Download our application!</p>
+<p><a href="LavatoryLocatorAlphaRelease.apk"</a>Download our application Alpha Phase!</p>
+<p><a href="LavatoryLocator.apk"</a>Download our application ZFR Phase!</p>
 </body>
 </html>
 
