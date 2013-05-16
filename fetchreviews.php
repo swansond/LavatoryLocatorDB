669e4dd499622fe2ecce0cdb02d88dabb23f33d1
aasav <aasav@cs.uw.edu>
2013-05-16 16:26:18 -0700

    Not returns a field uservote set to indicate user's helpfulness vote

--- fetchreviews.php
+++ fetchreviews.php
@@ -13,6 +13,7 @@ $lid =        $_GET['lid'];
 $pageNo =     $_GET['pageNo'];
 $sortMethod = $_GET['sortparam'];
 $sortDir =    $_GET['direction'];
+$uid =        $_GET['uid'];
 
 if (!$lid || !$pageNo || !$sortMethod || !$sortDir) {
     header('HTTP/1.1 400 Invalid Request');
@@ -68,6 +69,27 @@ while ($next = pg_fetch_array($result, NULL, PGSQL_ASSOC)) {
         'review' => $next['review'],
         'rating' => $next['rating'],
         'helpfulness' => $next['helpfulness']);
+    // If a user ID was supplied, see if they marked this review (un)helpful
+    if (isset($_GET['uid'])) {
+        $rid = $newEntry['rid'];
+        $query = "SELECT helpfulness FROM helpful
+                  WHERE user_id=$uid AND review_id=$rid";
+        $userResult = pg_query($db, $query);
+        if (!$userResult) {
+            header('HTTP/1.1 500 Server Error');
+            die('HTTP/1.1 500 Server Error: unable to query the server');
+        }
+        $userArr = pg_fetch_assoc($userResult);
+        $userVote = 0;
+        if (count($userArr) != 0) {
+            if ($userArr['helpfulness']) {
+                $userVote = 1;
+          } else {
+                $uservote = -1;
+          }
+        }
+        $newEntry['uservote'] = $userVote;
+    }
     array_push($returnArr['reviews'], $newEntry);
 }
 
