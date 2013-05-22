386d36633325de412d3ad6bd847b4004151d4384
David Jung <xdavidjung@gmail.com>
2013-05-21 18:08:54 -0700

    adding the beta download link

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 </head>
 <body>
 <h1>Lavatory Locator</h1>
-<p><a href="LavatoryLocatorAlphaRelease.apk"</a>Download our application Alpha Phase!</p>
-<p><a href="LavatoryLocator.apk"</a>Download our application ZFR Phase!</p>
+<p><a href="LavatoryLocatorBeta.apk"</a>Download our application Beta Phase release!</p>
+<p><a href="LavatoryLocatorAlphaRelease.apk"</a>Download our application Alpha Phase release!</p>
+<p><a href="LavatoryLocator.apk"</a>Download our application ZFR Phase release!</p>
 </body>
 </html>
 
