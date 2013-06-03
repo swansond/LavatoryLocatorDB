3ab4e34dcbf5f6cda91b1de6b3c9e24c4881ef81
David Jung <xdavidjung@gmail.com>
2013-06-02 23:15:21 -0700

    adding link to v1 on index; emma coverage report

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 </head>
 <body>
 <h1>Lavatory Locator</h1>
+<p><a href="LavatoryLocatorv1.apk"</a>Download our application V1 Phase release!</p>
 <p><a href="LavatoryLocatorBeta.apk"</a>Download our application Beta Phase release!</p>
 <p><a href="LavatoryLocatorAlphaRelease.apk"</a>Download our application Alpha Phase release!</p>
 <p><a href="LavatoryLocator.apk"</a>Download our application ZFR Phase release!</p>
