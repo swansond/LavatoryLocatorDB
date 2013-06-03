84bab6d354a689f5b090937fe7c70728b8d08561
David Jung <xdavidjung@gmail.com>
2013-06-02 23:05:10 -0700

    fixed up the page with the new file locations/names

---
+++ loadtestresults.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+</head>
+<body>
+    <h1>Lavatory Locator: Load Test Results</h1>
+    <dl>
+        <dt>Server Test</dt>
+        <dd>
+            <ul>
+                <li><a href="loadtests/servertest/servertest5.csv">Overall Server - 5 Threads</a></li>
+                <li><a href="loadtests/servertest/servertest10.csv">Overall Server - 10 Threads</a></li>
+                <li><a href="loadtests/servertest/servertest22.csv">Overall Server - 22 Threads</a></li>
+            </ul>
+        </dd>
+        
+        <dt>addlava Test</dt>
+        <dd>
+            <ul>
+                <li><a href="loadtests/addlava/addlava22.csv">addlava - 22 Threads</a></li>
+                <li><a href="loadtests/addlava/addlava100.csv">addlava - 100 Threads</a></li>
+                <li><a href="loadtests/addlava/addlava200.csv">addlava - 200 Threads</a></li>
+            </ul>
+        </dd>
+        
+        <dt>deletelava Test</dt>
+        <dd>
+            <ul>
+                <li><a href="loadtests/deletelava/deletelava22.csv">deletelava - 22 Threads</a></li>
+                <li><a href="loadtests/deletelava/deletelava100.csv">deletelava- 100 Threads</a></li>
+                <li><a href="loadtests/deletelava/deletelava200.csv">deletelava - 200 Threads</a></li>
+            </ul>
+        </dd>
+        
+        <dt>fetchreviews Test</dt>
+        <dd>
+            <ul>
+                <li><a href="loadtests/fetchreviews/fetchreviews22.csv">fetchreviews- 22 Threads</a></li>
+                <li><a href="loadtests/fetchreviews/fetchreviews100.csv">fetchreviews - 100 Threads</a></li>
+                <li><a href="loadtests/fetchreviews/fetchreviews200.csv">fetchreviews - 200 Threads</a></li>
+            </ul>
+        </dd>
+        
+        <dt>fetchuserreview Test</dt>
+        <dd>
+            <ul>
+                <li><a href="loadtests/fetchuserreview/fetchuserreview22.csv">fetchuserreview - 22 Threads</a></li>
+                <li><a href="loadtests/fetchuserreview/fetchuserreview100.csv">fetchuserreview - 100 Threads</a></li>
+                <li><a href="loadtests/fetchuserreview/fetchuserreview200.csv">fetchuserreview - 200 Threads</a></li>
+            </ul>
+        </dd>
+        
+        <dt>got2go Test</dt>
+        <dd>
+            <ul>
+                <li><a href="loadtests/got2go/got2go22.csv">got2go - 22 Threads</a></li>
+                <li><a href="loadtests/got2go/got2go100.csv">got2go - 100 Threads</a></li>
+                <li><a href="loadtests/got2go/got2go200.csv">got2go - 200 Threads</a></li>
+            </ul>
+        </dd>
+        
+        <dt>lavasearch Test</dt>
+        <dd>
+            <ul>
+                <li><a href="loadtests/lavasearch/lavasearch22.csv">lavasearch - 22 Threads</a></li>
+                <li><a href="loadtests/lavasearch/lavasearch100.csv">lavasearch - 100 Threads</a></li>
+                <li><a href="loadtests/lavasearch/lavasearch200.csv">lavasearch - 200 Threads</a></li>
+            </ul>
+        </dd>
+        
+        <dt>submithelpful Test</dt>
+        <dd>
+            <ul>
+                <li><a href="loadtests/submithelpful/submithelpful22.csv">submithelpful - 22 Threads</a></li>
+                <li><a href="loadtests/submithelpful/submithelpful100.csv">submithelpful - 100 Threads</a></li>
+                <li><a href="loadtests/submithelpful/submithelpful200.csv">submithelpful - 200 Threads</a></li>
+            </ul>
+        </dd>
+        
+        <dt>submitreview Test</dt>
+        <dd>
+            <ul>
+                <li><a href="loadtests/submitreview/submitreview22.csv">submitreview - 22 Threads</a></li>
+                <li><a href="loadtests/submitreview/submitreview100.csv">submitreview - 100 Threads</a></li>
+                <li><a href="loadtests/submitreview/submitreview200.csv">submitreview - 200 Threads</a></li>
+            </ul>
+        </dd>
+        
+        <dt>updatelava Test</dt>
+        <dd>
+            <ul>
+                <li><a href="loadtests/updatelava/updatelava22.csv">updatelava - 22 Threads</a></li>
+                <li><a href="loadtests/updatelava/updatelava100.csv">updatelava - 100 Threads</a></li>
+                <li><a href="loadtests/updatelava/updatelava200.csv">updatelava - 200 Threads</a></li>
+            </ul>
+        </dd>
+    </dl>
+</body>
+</html>
