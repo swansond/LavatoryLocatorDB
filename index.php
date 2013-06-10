0b79fbcfbf2e78e832359c6b0604379233b94b29
David Jung <xdavidjung@gmail.com>
2013-06-09 22:16:56 -0700

    added the v2 apk

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 </head>
 <body>
 <h1>Lavatory Locator</h1>
+<p><a href="LavatoryLocatorv2.apk"</a>Download our application V2 Phase release!</p>
 <p><a href="LavatoryLocatorv1.apk"</a>Download our application V1 Phase release!</p>
 <p><a href="LavatoryLocatorBeta.apk"</a>Download our application Beta Phase release!</p>
 <p><a href="LavatoryLocatorAlphaRelease.apk"</a>Download our application Alpha Phase release!</p>
